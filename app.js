706e91a142191d852fb4ab0a454730056f196b01
Abubakar Shaikh | Nexenstial Account <155366076+abubakar-nexenstial@users.noreply.github.com>
2024-01-08 10:48:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var path = require('path');
+import express from 'express';
+import path from 'path';
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,22 +11,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 8080;
 
-var router = express.Router();
+const router = express.Router();
 
-router.get('/', function (request, response) {
+router.get('/', (request, response) => {
   response.render('index', { title: 'Welcome!' });
 });
 
-router.get('/student', function (request, response) {
+router.get('/student', (request, response) => {
   response.render('index', { title: 'Welcome, student!' });
 });
 
-router.get('/teacher', function (request, response) {
+router.get('/teacher', (request, response) => {
   response.render('index', { title: 'Welcome, teacher!' });
 });
 
 app.use('/', router);
 
-app.listen(PORT, function () {
-  console.log('Listening on port ' + PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
